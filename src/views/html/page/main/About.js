11aace6fa0b7c3826588719da32f1c3f84bcdb5d
kimhyera <khr1828@naver.com>
2025-02-14 22:39:02 +0900

    build

--- src/views/html/page/main/About.js
+++ src/views/html/page/main/About.js
@@ -4,13 +4,39 @@ function About() {
   return (
     <>
       <main className="p_main">
-        <br /> <br /> <br /> <br /> <br /> <br />
-        About 10년 경력 퍼블리셔, UI 퍼포먼스를 극대화하는 개발자
+			Hello, I'm Coder 
+        About 10년 경력, <br /><br />
+				200여 건의 프로젝트 경험있는 퍼블리셔
+				<br />
+				 UI 퍼포먼스를 극대화하는 개발자
         <br />
         ✅ 10년 차 웹 퍼블리셔, 다양한 UI/UX 프로젝트 경험
         <br />
         ✅ Vue/Nuxt 기반 프론트엔드 개발 경험
         <br />✅ 반응형 & 접근성 최적화 작업 다수 진행
+
+
+				Hello, I'm Coder
+
+200여 건의 프로젝트 경험있는 퍼블리셔
+Contact me
+About me
+
+ 에이전시에서 200여 건을 작업한 경험으로 웹에서 보여지는 모든사이트 완벽하게 작업가능합니다.
+ 어드민,고객관리, 제작가능합니다.
+ 반응형에 최적화된 최대한 가볍고 빠르게 제작가능합니다.
+ 클라이언트가 이해할 수 있는 용어로 커뮤니케이션을 수월하게 진행합니다.
+Skills
+
+ HTML5
+ SCSS/CSS
+ Javascript/jQuery
+ 크롬, 사파리, IE10 크로스브라우징(협의를 통한 IE8 ,IE9도 가능)
+ 웹접근성 준수 및 인증마크 획득한 경험 유
+Contact
+
+ Tel: 010-6579-1828
+ Email: khr1828@gmail.com
       </main>
     </>
   );
